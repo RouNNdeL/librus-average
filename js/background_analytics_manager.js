9ab2a2db030257394f24204e7a724b444bb04b00
RouNdeL <krzys.zdulski@gmail.com>
2019-02-04 20:21:11 +0100

    Fix event label for content script event in Firefox

--- js/background_analytics_manager.js
+++ js/background_analytics_manager.js
@@ -6,12 +6,19 @@
 function onMessageListener(request, sender, sendResponse) {
     switch(request[MSG_COM]) {
         case CONTENT_SCRIPT_LOADED: {
+            let url;
+            if(sender.tab !== undefined && sender.tab.url !== undefined)
+                url = sender.tab.url;
+            else if(sender.url !== undefined)
+                url = sender.url;
+            else
+                url = "no_url";
             //noinspection JSUnresolvedVariable
             ga('send', {
                 hitType: 'event',
                 eventCategory: 'Content Script',
                 eventAction: 'pageLoad',
-                eventLabel: sender.tab ? sender.tab.url : "extension"
+                eventLabel: url
             });
         }
     }
